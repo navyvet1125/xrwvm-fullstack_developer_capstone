82af64ed6d1c5a4d84cc2c26a31d097b2099ae83
Evan Washington <enavy04@gmail.com>
2024-05-29 21:33:26 -0700

    registered models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,8 @@
-# from django.contrib import admin
-# from .models import related models
+"""Admin configuration for the djangoapp app."""
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
-
-# CarModelInline class
-
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+admin.site.register(CarMake)
+admin.site.register(CarModel)
