ec819a990419de10322c84a0ba1265b743de0836
Evan Washington <enavy04@gmail.com>
2024-05-29 21:33:13 -0700

    created models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,56 @@
-# Uncomment the following imports before adding the Model code
+"""Django models for the CarMake and CarModel classes."""
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+class CarMake(models.Model):
+    """Car Make class"""
+    name = models.CharField(null=False, max_length=100)
+    description = models.CharField(null=False, max_length=1000)
+    id = models.AutoField(primary_key=True)
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return f'{self.name}: {self.description}'
 
+class CarModel(models.Model):
+    """Car Model class"""
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=100)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('HATCHBACK', 'Hatchback'),
+        ('SPORTS', 'Sports'),
+        ('TRUCK', 'Truck'),
+        ('VAN', 'Van'),
+        ('CONVERTIBLE', 'Convertible'),
+        ('COUPE', 'Coupe'),
+        ('MOTORCYCLE', 'Motorcycle'),
+        ('MINIVAN', 'Minivan'),
+        ('PICKUP', 'Pickup'),
+        ('CROSSOVER', 'Crossover'),
+        ('OTHER', 'Other'),
+        ]
+    type = models.CharField(null=False, max_length=12, choices=CAR_TYPES, default='SEDAN')
+    year = models.IntegerField(default=2023,
+        validators=[
+            MinValueValidator(2015), 
+            MaxValueValidator(2023)
+        ])
+    
+    CAR_STATUS = [
+        ('AVAILABLE', 'Available'),
+        ('SOLD', 'Sold'),
+        ('REPAIR', 'Repair'),
+        ('IN_TRANSIT', 'In Transit'),
+        ('HOLD', 'Hold')
+        ]
+    status = models.CharField(null=False, max_length=10, choices=CAR_STATUS, default='AVAILABLE')
+    dateAdded = models.DateTimeField(default=now, editable=False)
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return f'{self.year} {self.car_make.name} {self.name} {self.type}'
