92a417c4f4b8d6aded89380409219bf92d452dd3
Evan Washington <enavy04@gmail.com>
2024-05-30 15:10:36 -0700

    linting fix

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -15,6 +15,7 @@ class CarMake(models.Model):
     def __str__(self):
         return f'{self.name}: {self.description}'
 
+
 class CarModel(models.Model):
     """Car Model class"""
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
@@ -35,7 +36,11 @@ class CarModel(models.Model):
         ('CROSSOVER', 'Crossover'),
         ('OTHER', 'Other'),
         ]
-    type = models.CharField(null=False, max_length=12, choices=CAR_TYPES, default='SEDAN')
+    type = models.CharField(
+        null=False,
+        max_length=12,
+        choices=CAR_TYPES,
+        default='SEDAN')
     year = models.IntegerField(default=2023,
         validators=[
             MinValueValidator(2015), 
@@ -51,6 +56,7 @@ class CarModel(models.Model):
         ]
     status = models.CharField(null=False, max_length=10, choices=CAR_STATUS, default='AVAILABLE')
     dateAdded = models.DateTimeField(default=now, editable=False)
+
 
     def __str__(self):
         return f'{self.year} {self.car_make.name} {self.name} {self.type}'
