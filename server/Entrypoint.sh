70a18d34c6a93b803f3fd63ba0c04184f81446c4
Evan Washington <enavy04@gmail.com>
2024-06-03 18:47:50 -0700

    fixed issue with car models not migrating

--- server/Entrypoint.sh
+++ server/Entrypoint.sh
@@ -3,6 +3,8 @@
 # Make migrations and migrate the database.
 echo "Making migrations and migrating the database. "
 python manage.py makemigrations --noinput
+python manage.py makemigrations djangoapp
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+python manage.py createsuperuser --noinput
 exec "$@"
